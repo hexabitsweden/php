b955ff0be9c77f198f01c6ee1946a9f29c05dd55
Daniel Jendinger <hexabit@MacBook-Pro.local>
2024-01-11 15:55:46 +0100

    blablabla

--- skrivare_temp2.py
+++ skrivare_temp2.py
@@ -1,4 +1,7 @@
 import requests
-payload = {'uname': 'tjalle', 'pass': 'tjalle'}
+from bs4 import BeautifulSoup as soup
+soup.original_encoding = 'utf-8'
+
+payload = {'uname': 'fredde', 'pass': 'fredde'}
 r = requests.post('http://localhost:9000/cuser.php', data=payload)
 print(r.status_code)
